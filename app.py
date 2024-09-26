004bd72952b7b930c45283bcfcec9d7da62f00fd
Manny <mhfernan35277@gmail.com>
2024-09-26 03:52:04 -0700

    commit

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 db.init_app(app)
 

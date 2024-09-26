c3f28a703b5489135e9b810bfb3d39fd092fb513
Manny <mhfernan35277@gmail.com>
2024-09-26 02:09:45 -0700

    initial

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import os
 from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
